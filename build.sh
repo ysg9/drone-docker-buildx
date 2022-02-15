f8e47b904f58be2d18ba8a6e3e85c3e28dab2cb1
yungsg <ysg@nus.edu.sg>
2022-02-15 12:41:31 +0800

    added build, drone files

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -x
+
+# update deps
+go mod tidy
+#go get golang.org/x/crypto
+
+go build -v -a -tags netgo -o release/amd64/drone-docker-buildx ./cmd/drone-docker-buildx/
+ls -l release/linux/amd64/drone-docker-buildx
+
+# for deps scanning
+go list -m all > go.list
+ls -l go.list
+
