e407602db60feda7d5f8de100b7a45d0d57337b0
yungsg <ysg@nus.edu.sg>
2022-02-15 15:10:41 +0800

    fix missing dir

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -x
 go mod tidy
 #go get golang.org/x/crypto
 
+mkdir -p release/amd64/
 go build -v -a -tags netgo -o release/amd64/drone-docker-buildx ./cmd/drone-docker-buildx/
 ls -l release/linux/amd64/drone-docker-buildx
 
